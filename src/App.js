f8c80b11074f496cad5dd9a2966ef7be8a79271e
Vadzim Zieńka <v@vadzim.info>
2019-02-08 21:54:44 +0300

    simplify code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { HotKeys } from "react-hotkeys";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 const keyMap = {
   ShiftBackspace: "shift+backspace",
 };
 
 const handlers = {
-  ShiftBackspace: () => alert('shift+backspace'),
+  ShiftBackspace: () => alert("shift+backspace"),
 };
 
-class App extends Component {
-  render() {
-    return (
-      <HotKeys keyMap={keyMap}>
-      <HotKeys handlers={handlers}>
+const App = () => (
+  <HotKeys keyMap={keyMap}>
+    <HotKeys handlers={handlers}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
-      </HotKeys>
-      </HotKeys>
-    );
-  }
-}
+    </HotKeys>
+  </HotKeys>
+);
 
 export default App;
